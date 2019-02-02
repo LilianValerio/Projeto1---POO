3aeb2bad4c9ff0234f181f4b00d66f9d908886d7
Lilian Santos <34148536+LilianValerio@users.noreply.github.com>
2019-02-02 15:12:46 -0200

    interface user

--- src/main/java/br/com/codenation/aceleradev/rec/loja/business/UserBusiness.java
+++ src/main/java/br/com/codenation/aceleradev/rec/loja/business/UserBusiness.java
@@ -1,14 +1,13 @@
 package br.com.codenation.aceleradev.rec.loja.business;
 
+import br.com.codenation.aceleradev.rec.loja.dao.UsuarioDao;
 import br.com.codenation.aceleradev.rec.loja.interfaces.UserInterface;
 
 public class UserBusiness  implements UserInterface{
 
 	public Boolean validarCpf(String cpf) {
-		
-		
-
-		return null;
+		UsuarioDao usuarioDao = new UsuarioDao();
+		return usuarioDao.validarCpf(cpf);
 	}
 
 }
--- src/main/java/br/com/codenation/aceleradev/rec/loja/dao/UsuarioDao.java
+++ src/main/java/br/com/codenation/aceleradev/rec/loja/dao/UsuarioDao.java
@@ -1,5 +1,10 @@
 package br.com.codenation.aceleradev.rec.loja.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.codenation.aceleradev.rec.loja.LojaSql;
 import br.com.codenation.aceleradev.rec.loja.conexaoBanco.ConexaoJDBC;
 
 public class UsuarioDao {
@@ -11,8 +16,18 @@ public class UsuarioDao {
 		
 		ConexaoJDBC conexao = new ConexaoJDBC();
 		conexao.getConexaoMySQL();
+		ResultSet resultado= null;
 		
+		PreparedStatement query = null;
+		try {
+		 resultado=  query.executeQuery(LojaSql.SELECT_USUARIO_CPF);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
+		if(resultado != null) {
+			existeCpf = true;
+		}
 		
 		
 		return existeCpf;
