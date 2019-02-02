e7ab7d2ad296e50f11a11beda96df01c11cd42fb
beatrizacbs <beatrizacbs@gmail.com>
2019-02-02 15:28:01 -0300

    alteração de tratamento de exceção no usuario

--- src/main/java/br/com/codenation/aceleradev/rec/loja/business/UserBusiness.java
+++ src/main/java/br/com/codenation/aceleradev/rec/loja/business/UserBusiness.java
@@ -1,13 +1,18 @@
 package br.com.codenation.aceleradev.rec.loja.business;
 
 import br.com.codenation.aceleradev.rec.loja.dao.UsuarioDao;
+import br.com.codenation.aceleradev.rec.loja.exceptions.CpfInvalidoException;
 import br.com.codenation.aceleradev.rec.loja.interfaces.UserInterface;
 
 public class UserBusiness  implements UserInterface{
 
-	public Boolean validarCpf(String cpf) {
+	public Boolean validarCpf(String cpf) throws CpfInvalidoException{
 		UsuarioDao usuarioDao = new UsuarioDao();
-		return usuarioDao.validarCpf(cpf);
+		if(usuarioDao.validarCpf(cpf)){
+			return true;
+		}else{
+			throw new CpfInvalidoException();
+		}
 	}
 
 }
