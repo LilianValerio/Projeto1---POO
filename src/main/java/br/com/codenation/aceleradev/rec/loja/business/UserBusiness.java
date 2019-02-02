eeea42661b2348ebded3d91b2b30c1a2be9f73e4
Lilian Santos <34148536+LilianValerio@users.noreply.github.com>
2019-02-02 14:33:29 -0200

    criacao das interfaces

---
+++ src/main/java/br/com/codenation/aceleradev/rec/loja/business/UserBusiness.java
@@ -0,0 +1,12 @@
+package br.com.codenation.aceleradev.rec.loja.business;
+
+import br.com.codenation.aceleradev.rec.loja.interfaces.UserInterface;
+
+public class UserBusiness  implements UserInterface{
+
+	public Boolean validarCpf(String cpf) {
+
+		return null;
+	}
+
+}
